b64f9f2b2cc418371726314a44044c63e93b9b77
AntoninB4440 <barceloantonin@gmail.com>
2021-04-27 15:34:39 +0200

    fix

--- src/app/web-service/user.service.ts
+++ src/app/web-service/user.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  baseUrl : string = 'http://localhost:4200/'
+
+  constructor(private http : HttpClient) { }
+
+  login(user : User){
+    return this.http.post<User>(this.baseUrl + "/user" , user);
+  }
+
+
+
+
+
 }
