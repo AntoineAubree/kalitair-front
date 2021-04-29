ac4faa3dcb13b8f96ea852eb0b3cc596b3e54dca
AntoninB4440 <barceloantonin@gmail.com>
2021-04-29 13:02:22 +0200

    create user form ok

--- src/app/user/user-create/confirmPassword.directive.ts
+++ src/app/user/user-create/confirmPassword.directive.ts
@@ -4,5 +4,5 @@ export const confirmPasswordValidator: ValidatorFn = (control: AbstractControl):
     const password = control.get('password');
     const confirmPassword = control.get('confirmPassword');
   
-    return  password?.value !== confirmPassword?.value ? { match: false } : null;
+    return  password?.value === confirmPassword?.value ? null : { match : true};
   };
