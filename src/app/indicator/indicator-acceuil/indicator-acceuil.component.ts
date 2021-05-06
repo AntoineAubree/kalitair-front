9ab6db91aad120b0fd9437a795faf7d576b7bd3d
AntoninB4440 <barceloantonin@gmail.com>
2021-05-06 16:46:30 +0200

    get latlon ok click map (bug on zoom button)

--- src/app/indicator/indicator-acceuil/indicator-acceuil.component.ts
+++ src/app/indicator/indicator-acceuil/indicator-acceuil.component.ts
@@ -3,15 +3,13 @@ import Map from 'ol/Map';
 import Tile from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import View from 'ol/View';
-import { fromLonLat } from 'ol/proj';
+import { fromLonLat, toLonLat, transform } from 'ol/proj';
 import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
 import { Feature } from 'ol';
 import Point from 'ol/geom/Point';
 import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon'
-import RegularShape from 'ol/style/RegularShape'
-import { size } from 'underscore';
 
 
 
@@ -25,9 +23,7 @@ export class IndicatorAcceuilComponent implements OnInit {
 
   map: any;
   markerSource: VectorSource = new VectorSource({});
-  marker: Feature = new Feature({
-    geometry: new Point(fromLonLat([2.213749,46.227638])) // dont worry about coordinate type 0,0 will be in west coast of africa
-  }); 
+  
   constructor() { }
 
   ngOnInit(): void {
@@ -45,8 +41,9 @@ export class IndicatorAcceuilComponent implements OnInit {
           source: this.markerSource,
           style: new Style({
             image: new Icon({
+              anchor : [0.5,1],
               src: 'assets/pin24x24.png',
-              scale : 1,
+              scale : 1.5,
               imgSize: [24,24],
             })
           })
@@ -54,13 +51,29 @@ export class IndicatorAcceuilComponent implements OnInit {
       ],
       view: new View({
         center: fromLonLat([2.213749,46.227638]),
-        zoom: 5
+        zoom: 6
       }),
     });
 
-    this.markerSource.addFeature(this.marker)
+  }
+
+  getCoord(event: any) {
+    const coordinate = this.map.getEventCoordinate(event);
+    let lonlat = transform(coordinate, 'EPSG:3857', 'EPSG:4326');
+    const lon = lonlat[0];
+    const lat = lonlat[1];
+    console.log(lonlat)
+
+    this.addMarker(lon, lat);
+  }
+
+  addMarker(lon: number, lat: number) {
+    this.markerSource.clear();
+    let marker = new Feature({
+      geometry: new Point(fromLonLat([lon,lat])) // dont worry about coordinate type 0,0 will be in west coast of africa
+    });
+    this.markerSource.addFeature(marker)
     
-  
   }
 
 }
